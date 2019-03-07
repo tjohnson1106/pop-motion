7224b5c6470fe8b2ba4126ee349d2e89581bfdbe
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-03-07 16:38:41 -0500

    created animated view properties

--- src/motion.js
+++ src/motion.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Animated } from "react-native";
 
 import { keyframes, stagger } from "popmotion";
 import { duration } from "moment";
 import { inflate } from "zlib";
+import Animated from "react-native-reanimated";
 
 const COUNT = 5;
 const DURATION = 4000;
@@ -14,6 +15,10 @@ class Motion extends Component {
   state = {
     animations: constructAnimations()
   };
+
+  componentDidMount() {
+    this.animateCircles();
+  }
 
   animateCircles = () => {
     const frames = Array(COUNT).fill(
@@ -37,8 +42,41 @@ class Motion extends Component {
   };
 
   render() {
-    return {};
+    return (
+      <View style={styles.root}>
+        {this.state.animations.map(({ opacity, state }, index) => {
+          return (
+            <Animated.View
+              key={index}
+              style={[
+                styles.circle,
+                {
+                  transform: [{ scale }],
+                  opacity
+                }
+              ]}
+            />
+          );
+        })}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff"
+  },
+  circle: {
+    backgroundColor: "gold",
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    position: "absolue"
+  }
+});
 
 export default Motion;
